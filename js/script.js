a97af4bdd9fb1047388d5374021b5eeeddb00e34
Xenon-ne <xenon.alin.ma@gmail.com>
2024-06-27 02:34:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const galleryItems = document.querySelectorAll('.gallery-item');
+document.addEventListener("DOMContentLoaded", function() {
+    const galleryItems = document.querySelectorAll(".gallery-item");
 
     galleryItems.forEach(item => {
-        item.addEventListener('click', () => {
-            if (item.classList.contains('fullscreen')) {
-                item.classList.remove('fullscreen');
-            } else {
-                galleryItems.forEach(i => i.classList.remove('fullscreen'));
-                item.classList.add('fullscreen');
-            }
-        });
-    });
+        item.addEventListener("click", () => {
+            const fullscreenContainer = document.createElement("div");
+            fullscreenContainer.classList.add("fullscreen");
 
-    const gallery = document.querySelector('.gallery');
-    gallery.addEventListener('scroll', () => {
-        const maxScrollLeft = gallery.scrollWidth - gallery.clientWidth;
-        if (gallery.scrollLeft === 0) {
-            gallery.scrollLeft = maxScrollLeft - 1;
-        } else if (gallery.scrollLeft === maxScrollLeft) {
-            gallery.scrollLeft = 1;
-        }
+            const img = document.createElement("img");
+            img.src = item.src;
+            fullscreenContainer.appendChild(img);
+
+            document.body.appendChild(fullscreenContainer);
+
+            fullscreenContainer.addEventListener("click", () => {
+                fullscreenContainer.remove();
+            });
+        });
     });
 });
